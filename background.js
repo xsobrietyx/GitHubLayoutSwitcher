fb463dec70b318272c93c9b26c46784e6a6441b2
xsobrietyx <vadym.serdiuk@gmail.com>
2019-04-06 18:43:24 +0300

    WIP, code cleanup.

--- background.js
+++ background.js
@@ -4,19 +4,26 @@
     A) https://github.com -> https://github.com/PROFILE_NAME
     B) https://gist.github.com/PROFILE_NAME -> https://gist.github.com/PROFILE_NAME/public
  */
-var callback = function (details) {
-    console.log("Intercepted url: " + details.url);
-    if (details.url.match(/^(.+(gist)).*(xsobrietyx)$/gm)) {
-        return {redirectUrl: details.url + "/public"};
-    } else if (details.url.match(/^(.+(gist)).*(xsobrietyx\/)$/gm)) {
-        return {redirectUrl: details.url + "public"};
-    } else if (details.url.match(/.+(github.com)$/gm)) {
-        return {redirectUrl: details.url + "/xsobrietyx"};
-    } else if (details.url.match(/.+(github.com\/)$/gm)) {
-        return {redirectUrl: details.url + "xsobrietyx"};
+var callback = function (request) {
+    var matchers = createMatchers("xsobrietyx"); //TODO move to the localStorage
+
+    for (var i = 1; i < matchers.length; i++) {
+        if (request.url.match(matchers[i][0])) {
+            return {redirectUrl: request.url + matchers[i][1]};
+        }
     }
+
     return {};
 };
+
+function createMatchers(a) {
+    var flags = "gm";
+    var cases = ["^(.+(gist)).*(" + a + ")$", "^(.+(gist)).*(" + a + "/)$", ".+(github.com)$", ".+(github.com/)$"];
+    return [a,
+        [new RegExp(cases[0], flags), "/public"], [new RegExp(cases[1], flags), "public"],
+        [new RegExp(cases[2], flags), "/" + a], [new RegExp(cases[3], flags), a]
+    ];
+}
 
 var filter = {urls: ["*://*.github.com/*"], types: ["main_frame"]};
 var options = ["blocking"];
